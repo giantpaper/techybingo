66d3ad5580ff0e244e6e09e597ef9f74f2ec837d
giantpaper <giantpaper@outlook.com>
2024-12-12 02:02:18 -0800

    IMPROV: pull content from ext. text file

--- src/assets/_default.js
+++ src/assets/_default.js
@@ -2,40 +2,27 @@ import { checkWeek, shuffle } from "./_functions.js"
 import { ref } from 'vue'
 import Progress from "./_progress.js"
 
-const items = ref([
-	`Joked about how long your "commute" is`,
-	`Used an emoji in an email`,
-	`Said "lol" and meant it`,
-	`Call interrupted by pet or family member`,
-	`Took a coffee break (or 2...or...3)`,
-	`Client is a no-show for a call`,
-	`Sent a gif to coworker(s)`,
-	`Created a workday playlist`,
-	`Did household chores on your lunchbreak`,
-	`Said or heard "Sorry, I was on mute"`,
-	`Said or heard "Can everyone see my screen?"`,
-	`Internet is having issues so you book it to a coffee shop`,
-	`Get asked to help with household chores during work`,
-	`Forgot to attach a file to an email`,
-	`Sudden power outage`,
-	`Used the pomodoro technique`,
-	`Said or heard "It's Fri-yay!"`,
-	`Said or heard "Does this need a quote?"`,
-	`You/Someone forgets what day it is.`,
-	`Forgot to update your Slack status from 'away' to 'active' after lunch`,
-	`Needed to restart after computer glitches/crashes`,
-	`Got up to refill your waterbottle`,
-	`Sent a meme to coworker(s)`,
-	`Client said "can we get on a call?"`,
-	`Someone says 'good morning!' to everyone`,
-	`Said or heard "Can you email that to everyone?" on a call`,
-	`Said or heard "Hey guys, I have to jump on another call"`,
-	`Said or heard "I think there's lag"`,
-	`*sound of someone typing on a call...possibly with a hammer*`,
-	`Said or heard "Can you email that to me?" on a call`,
-	`You/Someone uses a custom background on a call`,
-	`Helpscout/Github/Shopify/Cloudflare/Gmail is down`,
-])
+const squares = await fetch(`/src/squares.txt`)
+		.then(response => response.text())
+		.then(data => {
+			return data.split("\n")
+		})
+
+const items = ref(squares)
+
+// const items = ref([
+// 	`Bikers at the Moose Lodge`,
+// 	`Got up to refill your water`,
+// 	`"axe me a high question"`,
+// 	`Another fire on WatchDuty`,
+// 	`Saw a spider`,
+// 	`Cat battle outside`,
+// 	`Sonic boom outside`,
+// 	`Saw a raccoon`,
+// 	`Healer in Marvel Rivals doesn't know how to heal`,
+// 	`References The Good Place`,
+// 	`Sudden power outage`,
+// ])
 
 export default function DefaultValues () {
 	let l = []
