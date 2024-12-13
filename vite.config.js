c23f127a1e923d4f790a4f229af958204d8d3e47
giantpaper <giantpaper@outlook.com>
2024-12-13 01:34:52 -0800

    FIX: Vite would error out when compiling for build mode

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+	build:{
+		// https://stackoverflow.com/questions/76616620/vite-refuses-to-use-the-correct-build-target-in-my-svelte-ts-project
+		target: "esnext", // or "es2019",
+	},
   plugins: [
     vue(),
     vueDevTools(),
