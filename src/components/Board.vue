1c12a8ce7d85989182964b243223bdde10a5485d
giantpaper <giantpaper@outlook.com>
2024-12-12 20:57:31 -0800

    FIX: issue where there would sometimes be 2 free spaces
    Adds a disabled free space right where it says "free space" in the list ref() value instead of in the 12 spot on the list.

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,8 +2,8 @@
 	<h1>Week of {{ bingodate.month() + " "+ bingodate.week() }}</h1>
 	<ul class="table">
 		<li v-for="(item, i) in list" :class="bingo.classes(i)" :ref="li => (liList[i] = li)">
-			<div class="free-space checked" v-if="i===12">
-				Free Space
+			<div class="free-space checked" v-if="item.label===`Free Space`">
+				FREE SPACE
 			</div>
 			<label v-else :class="{ checked: item.checked, }">
 				<input type="checkbox" v-model="item.checked" @click="ifWin(list, i, 'clicked')" v-if="disabled===false" />
