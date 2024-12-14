18a6ed074956358eca69f59325d794cb62a23f96
giantpaper <giantpaper@outlook.com>
2024-12-13 20:50:17 -0800

    IMPROV: added new win condition - blackout

--- src/components/WinCond.vue
+++ src/components/WinCond.vue
@@ -3,16 +3,20 @@
 	<section class="win_cond">
 		<h2>Win Conditions:</h2>
 		<figure>
-			<img src="@/assets/win_row.svg" width="100" height="100" alt="" class="win_row" />
+			<img src="@/assets/win_row.svg" width="75" height="75" alt="" class="win_row" />
 			<figcaption>Any row</figcaption>
 		</figure>
 		<figure>
-			<img src="@/assets/win_col.svg" width="100" height="100" alt="" class="win_col" />
+			<img src="@/assets/win_col.svg" width="75" height="75" alt="" class="win_col" />
 			<figcaption>Any column</figcaption>
 		</figure>
 		<figure>
-			<img src="@/assets/win_diag.svg" width="100" height="100" alt="" class="win_diag" />
+			<img src="@/assets/win_diag.svg" width="75" height="75" alt="" class="win_diag" />
 			<figcaption>Any diagonal</figcaption>
+		</figure>
+		<figure>
+			<img src="@/assets/win_blackout.svg" width="75" height="75" alt="" class="win_diag" />
+			<figcaption>Blackout</figcaption>
 		</figure>
 	</section>
 </template>
@@ -21,10 +25,10 @@
 		display: flex;
 		flex-wrap: wrap;
 		gap: 1rem;
-		width: 50ch;
+		width: 60ch;
 		justify-content: space-between;
 		figure {
-			width: calc( (100% / 3) - 2rem );
+			width: calc( (100% / 4) - 2rem );
 		}
 		img {
 			margin: 0 auto;
