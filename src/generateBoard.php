8e7d71bf20ed48d9ae283763daaaac6cc6cc9c8e
giantpaper <giantpaper@outlook.com>
2024-12-21 00:05:27 -0800

    FIX: create a list for this week if there is none This (hopefully) fixes the board looking jank when deployed.

--- src/generateBoard.php
+++ src/generateBoard.php
@@ -7,6 +7,7 @@ $list = array_map('trim', file(FILE));
 shuffle($list);
 $list = array_slice($list, 0, 24);
 
+$sunday__this = date('Ymd',strtotime('last Sunday'));
 $sunday__next = date('Ymd',strtotime('next Sunday'));
 
 // Get last week's squares
@@ -14,6 +15,12 @@ $sunday__prev = date('Ymd',strtotime('2 weeks ago Sunday'));
 
 file_put_contents(txt($sunday__next), implode("\n", $list));
 
+if (file_exists(txt($sunday__next)) === false) {
+	file_put_contents(txt($sunday__next), implode("\n", $list));
+}
+if (file_exists(txt($sunday_this)) === false) {
+	file_put_contents(txt($sunday__this), implode("\n", $list));
+}
 if (file_exists(txt($sunday__prev)) !== false) {
 	unlink(txt($sunday__prev));
 }
