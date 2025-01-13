6625768d94f877153fe3549ad7ab39bbe3d2e2f2
giantpaper <giantpaper@outlook.com>
2025-01-13 02:10:06 -0800

    REVERT: everything how it needs to be

--- src/assets/_default.js
+++ src/assets/_default.js
@@ -34,18 +34,23 @@ function formatFileName(array) {
 	return str
 }
 
-const squares = await fetch(`./ready/${formatFileName(boards.thisWeeks)}.txt`)
+const url = `./ready/${formatFileName(boards.thisWeeks)}.txt`
+
+const response = await fetch(url)
+
+const squares = await fetch(url)
 		.then(response => response.text())
 		.then(data => data.replace(/\n$/, '').split("\n"))
 		.catch(err => console.error(err))
+
+const MODE = import.meta.env.VITE_MODE
 
 export default function DefaultValues () {
 	let l = []
 	let i = 0
 
-	if (typeof squares !== 'array' || (squares.length !== 24 && squares[0].match(/^</))) {
+	if (MODE === 'production' && response.status === 404 || (MODE === 'development' && squares.length !== 24 && squares[0].match(/^</))) {
 		console.error(`RUH RUH: Cannot find ./ready/${formatFileName(boards.thisWeeks)}.txt`, squares)
-		console.debug(typeof squares, (squares.length !== 24 && squares[0].match(/^</)))
 		return false
 	}
 
